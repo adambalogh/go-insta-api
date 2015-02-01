2ec36128b886dd38f6cb328e2064cf37597b155c
Adam Balogh <balogh.adam@icloud.com>
2015-02-01 15:07:27 +0000

    support follows

---
+++ insta/relationships.go
@@ -0,0 +1,20 @@
+package insta
+
+import (
+	"fmt"
+	"errors"
+)
+
+// GetFollows returns the users that are followed by the requested user
+func (i *InstaClient) GetFollows(userID string) ([]UserWithFullName, error) {
+	if len(userID) == 0 {
+		return nil, errors.New("User ID cannot be empty")
+	}
+	
+	followsResponse := new(FollowsResult)
+	err := i.getRequest(fmt.Sprintf("/users/%s/follows", userID), map[string]string{}, followsResponse)
+	if err != nil {
+		return nil, err
+	}
+	return followsResponse.Users, err
+}

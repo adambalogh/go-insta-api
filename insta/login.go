a2315bc71d95791b24dc0a70a3273681aaf90bb9
Adam Balogh <balogh.adam@icloud.com>
2015-03-03 18:31:58 +0000

    use x/oauth2

--- insta/login.go
+++ insta/login.go
@@ -1,16 +1,14 @@
 package insta
 
 import (
-	"encoding/json"
-	"errors"
-	"fmt"
 	"net/http"
-	"net/url"
+
+	"golang.org/x/oauth2"
 )
 
 const (
 	// Instagram login page URL
-	authorizationURL = "https://api.instagram.com/oauth/authorize/?client_id=%s&redirect_uri=%s&response_type=code"
+	authorizationURL = "https://api.instagram.com/oauth/authorize"
 	// Exchange code for access token URL
 	accessTokenURL = "https://api.instagram.com/oauth/access_token"
 )
@@ -22,50 +20,43 @@ type InstaLogin struct {
 	ClientID     string
 	ClientSecret string
 	RedirectURL  string
+	Conf         *oauth2.Config
+	Context      *oauth2.Context
 }
 
 // NewInstaLogin returns an initialized InstaLogin, with a SimpleHTTPRequester
-func NewInstaLogin(clientID, clientSecret, redirectURL string) *InstaLogin {
+func New(clientID, clientSecret, redirectURL string) *InstaLogin {
 	login := new(InstaLogin)
 	login.HTTPClient = &http.Client{}
 	login.ClientID = clientID
 	login.ClientSecret = clientSecret
 	login.RedirectURL = redirectURL
+	login.Conf = &oauth2.Config{
+		ClientID:     login.ClientID,
+		ClientSecret: login.ClientSecret,
+		Scopes:       []string{"basic"},
+		RedirectURL:  login.RedirectURL,
+		Endpoint: oauth2.Endpoint{
+			AuthURL:  authorizationURL,
+			TokenURL: accessTokenURL,
+		},
+	}
+	// TODO allow custom context
+	login.Context = &oauth2.NoContext
 	return login
 }
 
 // GetLoginURL returns Instagram login page's URL
 func (i *InstaLogin) GetLoginURL() string {
-	return fmt.Sprintf(authorizationURL, i.ClientID, i.RedirectURL)
+	return i.Conf.AuthCodeURL("test", oauth2.AccessTypeOnline)
 }
 
 // ExchangeCodeForAccessToken exchanges the code received from Instagram's Login page
 // for an access token
-func (i *InstaLogin) ExchangeCodeForAccessToken(code string) (string, error) {
-	// HTTP POST form values required for code exchange
-	params := url.Values{}
-	params.Add("code", code)
-	params.Add("client_id", i.ClientID)
-	params.Add("client_secret", i.ClientSecret)
-	params.Add("grant_type", "authorization_code")
-	params.Add("redirect_uri", i.RedirectURL)
-	// Send HTTP POST request
-	resp, err := i.HTTPClient.PostForm(accessTokenURL, params)
+func (i *InstaLogin) Exchange(code string) (string, error) {
+	accessToken, err := i.Conf.Exchange(i.Context, code)
 	if err != nil {
 		return "", err
 	}
-	// Check status code
-	if resp.StatusCode != 200 {
-		return "", errors.New("Failed to authenticate")
-	}
-
-	// Decode JSON to get AccessToken
-	decoder := json.NewDecoder(resp.Body)
-	var accessToken AccessTokenResponse
-	err = decoder.Decode(&accessToken)
-	if err != nil {
-		return "", err
-	}
-
 	return accessToken.AccessToken, nil
 }
